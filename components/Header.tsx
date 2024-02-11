2e1c97774a3fc6848ad14a6be69e03c5d984a9ca
ziaul888 <ziaul935@gmail.com>
2024-02-11 20:56:11 +0600

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='z-[990] relative'>
+        <div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40
+          bg-white bg-opacity-80'>
+         {/* <h1>hello</h1> */}
+        </div>
+    </header>
+  )
+}
+export default Header
