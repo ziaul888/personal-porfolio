d50726f7365031b6483cbc4e9846b51397e199a1
ziaul6 <ziaul935@gmail.com>
2024-02-12 00:06:24 +0600

    header added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Intro from "@/components/Intro";
 
 export default function Home() {
   return (
     <main className="">
-      
+      <Intro/>
     </main>
   )
 }
