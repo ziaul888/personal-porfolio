2867db80e5962b352a3441278002b0d3a8d0ef79
ziaul888 <ziaul935@gmail.com>
2024-02-12 23:37:35 +0600

    intro section done

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import Image from "next/image"
 import myImage from "../public/zia-ul-Hoque-bhuiyan.jpg"
 import {motion} from "framer-motion"
+import Link from 'next/link';
+import {BsArrowRight, BsGithub, BsLinkedin} from "react-icons/bs"
+import { LuHardDriveDownload } from 'react-icons/lu';
 
 const Intro = () => {
     return (
-        <section>
+        <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div
@@ -51,6 +54,33 @@ const Intro = () => {
                 building <span className="italic">sites & apps</span>. My focus is{" "}
                 <span className="underline">React (Next.js)</span>.
             </motion.h1>
+            <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 text-lg font-medium'
+             initial={{opacity: 0, y: 100}}
+             animate={{opacity: 1, y: 0}}
+             transition={{
+                delay:0.1
+             }}
+            >
+                <Link href="#contact" className='group bg-gray-900 text-white px-7 py-5 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>
+                    Contact me here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition'/>
+                </Link>
+                <a className='group bg-white px-7 py-5 flex items-center gap-2 rounded-full
+                outline-none focus:scale-110 hover:scale-110  active:scale-105 transition
+                cursor-pointer border border-black/10' href='/CV.pdf' download>Download CV 
+                    <LuHardDriveDownload className='opacity-60 group-hover:translate-y-1 transition'/>
+                </a>
+                <a className='bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full
+                focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950  active:scale-105 transition
+                cursor-pointer border border-black/10
+                ' href='https://www.linkedin.com' target='_blank'>
+                    <BsLinkedin/>
+                </a>
+                <a className='bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full
+                 focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition
+                cursor-pointer border border-black/10' href='https://github.com/ziaul888' target='_blank'>
+                    <BsGithub/>
+                </a>
+            </motion.div>
         </section>
     );
 };
