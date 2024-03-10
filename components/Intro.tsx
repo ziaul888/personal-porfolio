acdb58f57ab898e3ea23f5f8d33ad811f716c86f
ziaul6 <ziaul935@gmail.com>
2024-03-10 23:17:48 +0600

    conatct section added

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -53,7 +53,7 @@ const Intro = () => {
                 initial={{opacity: 0, y: 100}}
                 animate={{opacity: 1, y: 0}}
             >
-                <span className="font-bold">Hello, Ziaul Hoque Bhuiyan.</span> I'm a{" "}
+                <span className="font-bold">Hello, I'm Ziaul Hoque Bhuiyan.</span> I'm a{" "}
                 <span className="font-bold">Frontend react js developer</span> with{" "}
                 <span className="font-bold">2+ years</span> of experience. I enjoy
                 building <span className="italic">sites & apps</span>. My focus is{" "}
