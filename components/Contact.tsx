d0350b50bcf8cef37e7e4266cb7f89de157ae345
ziaul888 <ziaul935@gmail.com>
2025-04-21 20:49:03 +0600

    email

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -35,7 +35,7 @@ export default function Contact() {
             <p className="text-gray-700 -mt-6 dark:text-white/80">
                 Please contact me directly at{" "}
                 <a className="underline" href="mailto:ziaul935@gmail.com.com">
-                    example@gmail.com
+                    ziaul935@gmail.com
                 </a>{" "}
                 or through this form.
             </p>
